402b2ab4b149c957e74b375ff216f4b10c920ece
Tonismar Régis Bernardo <tonismar@gmail.com>
2025-07-21 11:29:05 -0300

    Clarify config
    Adicionado as configurações para teste a aplicação clarify.

--- app.py
+++ app.py
@@ -97,6 +97,16 @@ def show_table(db):
         )
     st.markdown("---")
 
+st.markdown("""
+  <script type="text/javascript">
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "sievgprfdx");
+  </script>
+""", unsafe_allow_html=True)
+
 st.title("Programação TV Claro")
 
 estados = st.selectbox(
